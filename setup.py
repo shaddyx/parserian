92d86fb7e0d14cbb03c810f69a257b2d1d261953
Anatolii Yakushko <shaddyx@gmail.com>
2024-02-11 03:40:46 +0200

    [+] added simple ratelimiter

--- setup.py
+++ setup.py
@@ -12,10 +12,11 @@ setup(
     # Needed for dependencies
     install_requires=[
                       'pytest',
+                      'cachetools',
                       ],
     package_data={"": ["*.json"]},
     # *strongly* suggested for sharing
-    version='0.05',
+    version='0.06',
     # The license can be anything you like
     license='MIT',
     description='A bunch of tools to help developing parsers',

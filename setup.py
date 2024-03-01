53b3fcf3d0fec5c5a95fc6f874d7271ab40303a6
Anatolii Yakushko <shaddyx@gmail.com>
2024-03-01 16:17:37 +0200

    [+] added find_packages

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools
 from setuptools import setup
 
 setup(
@@ -7,8 +8,8 @@ setup(
     author='Anatolii Yakushko',
     author_email='shaddyx@gmail.com',
     # Needed to actually package something
-    packages=['parserian'],
-    package_dir={'parserian': 'src/parserian'},
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
     # Needed for dependencies
     install_requires=[
         'pytest',
@@ -19,7 +20,7 @@ setup(
     ],
     package_data={"": ["*.json"]},
     # *strongly* suggested for sharing
-    version='0.10',
+    version='0.12',
     # The license can be anything you like
     license='MIT',
     description='A bunch of tools to help developing parsers',
